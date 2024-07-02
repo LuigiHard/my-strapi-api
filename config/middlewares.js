c26b66624f91e09326678d3c346643deb5db7559
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-07-02 13:34:03 -0300

    chore: Update CORS settings for development environment

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,31 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['http://localhost:5173', 'https://circoteste-810b29740124.herokuapp.com'], // Add your frontend URLs here
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
-});
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173', 'https://circoteste-810b29740124.herokuapp.com'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
