3681d8978071a59d361997ac4e0abb395c8bfed8
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-07-03 10:51:57 -0300

    feat: Add lazy loading for product images in product controller

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,34 @@
 'use strict';
 
 /**
- * product controller
+ *  product controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+    const products = await strapi.entityService.findMany('api::product.product', {
+      ...query,
+      populate: { image: true },
+    });
+
+    const formattedProducts = products.map(product => ({
+      id: product.id,
+      attributes: {
+        title: product.title,
+        company: product.company,
+        description: product.description,
+        featured: product.featured,
+        category: product.category,
+        image: product.image ? product.image.url : null,
+        price: product.price,
+        shipping: product.shipping,
+        colors: product.colors,
+      }
+    }));
+
+    return { data: formattedProducts, meta: { pagination: ctx.response.get('X-Total-Count') } };
+  }
+}));
